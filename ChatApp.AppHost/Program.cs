a2936a4ed9fdcbf00c0b3a47de417cb5df84d36e
Karol Błażejczyk <blazkaro@proton.me>
2025-03-22 15:50:43 +0100

    Initialized SPA project

--- ChatApp.AppHost/Program.cs
+++ ChatApp.AppHost/Program.cs
@@ -1,9 +1,26 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.ChatApp_RealTimeCommunication>("chatapp-realtimecommunication");
+var conversations = builder.AddProject<Projects.ChatApp_Conversations>("chatapp-conversations");
 
-builder.AddProject<Projects.ChatApp_Conversations>("chatapp-conversations");
+var rtc = builder.AddProject<Projects.ChatApp_RealTimeCommunication>("chatapp-realtimecommunication")
+    .WithReference(conversations)
+    .WaitFor(conversations);
 
-builder.AddProject<Projects.ChatApp_Messages>("chatapp-messages");
+var messages = builder.AddProject<Projects.ChatApp_Messages>("chatapp-messages")
+    .WithReference(conversations)
+    .WaitFor(conversations);
+
+var spaServer = builder.AddProject<Projects.ChatApp_Spa_Server>("chatapp-spa-server")
+    .WithReference(rtc)
+    .WithReference(conversations)
+    .WithReference(messages)
+    .WaitFor(rtc)
+    .WaitFor(conversations)
+    .WaitFor(messages);
+
+builder.AddNpmApp("chatapp-spa-client", "../Spa/src/chatapp.spa.client")
+    .WithReference(spaServer)
+    .WaitFor(spaServer)
+    .WithHttpsEndpoint(env: "DEV_ANGULAR_SPA_PORT");
 
 builder.Build().Run();
